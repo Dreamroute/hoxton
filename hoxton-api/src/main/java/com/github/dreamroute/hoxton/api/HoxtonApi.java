6fb351172fbcc6fe3f8386b1fc0f438f27d725c9
w.dehai <342252328@qq.com>
2020-09-16 12:33:13 +0800

    init2.

--- hoxton-api/src/main/java/com/github/dreamroute/hoxton/api/HoxtonApi.java
+++ hoxton-api/src/main/java/com/github/dreamroute/hoxton/api/HoxtonApi.java
@@ -1,12 +1,15 @@
 package com.github.dreamroute.hoxton.api;
 
+import cn.hutool.extra.servlet.ServletUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.ServletRequest;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
@@ -24,7 +27,8 @@ public class HoxtonApi {
     }
 
     @RequestMapping("/health")
-    public boolean health() {
+    public boolean health(ServletRequest request) {
+        String body = ServletUtil.getBody(request);
         System.err.println(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd:hh:mm:ss")));
         return true;
     }
